fe56caf374f4f996dbb64d35910979f4654f2a33
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-09-01 10:45:01 -0500

    Can display text on OLED screen
    First successful test to display text on the Adafruit P326D 128x64 OLED screen via Qwiik connector.

--- OLEDScreen/OLEDScreen.ino
+++ OLEDScreen/OLEDScreen.ino
@@ -1,6 +1,30 @@
-void setup() {
-  // put your setup code here, to run once:
+// Libraries
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+#include <Adafruit_GFX.h>
 
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define SCREEN_ADDRESS 0x3D
+#define OLED_RESET 13
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire1, OLED_RESET);
+
+const String TEST_STRING = "Successful test!";
+
+void displayText(String string){
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println(string);
+  display.display();
+};
+
+void setup() {
+  Serial.begin(115200);
+  display.begin();
+  displayText(TEST_STRING);
 }
 
 void loop() {
