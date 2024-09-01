c7ab4fef3b5480a202c22892132fa6c83862edf3
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-09-01 11:07:03 -0500

    Creating Python program to convert bitmaps
    The OLED screen needs certain format for bitmap data. This Python program will automatically format the data accordingly.

---
+++ Bitmap Conversion/bitmap_conversion.py
@@ -0,0 +1,8 @@
+#-- TODO: Write conversion software to convert bitmap images to screen-readable code
+#-- TODO: Learn how to communicate with Arduino over serial connection (and maybe wifi?)
+#-- TODO: Possibly write code to automatically convert any image to correct size and 1-bit color depth and push to screen
+
+from PIL import Image
+
+if __name__ == "__main__":
+    pass
