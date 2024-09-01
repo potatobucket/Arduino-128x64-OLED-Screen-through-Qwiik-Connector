0bba5a161a221b25cd752e1debc930560ace9dc0
potatobucket <104957727+potatobucket@users.noreply.github.com>
2024-09-01 11:53:53 -0500

    Separated pixel data into groups of 8
    So I can convert them to 8-bit binary numbers.

--- Bitmap Conversion/bitmap_conversion.py
+++ Bitmap Conversion/bitmap_conversion.py
@@ -4,5 +4,27 @@
 
 from PIL import Image
 
+testImage = "OLEDScreen/Example Bitmaps/potato_bitmap.bmp"
+
+class BitmapImage:
+    def __init__(self, image: str, pixelData: list = []):
+        self.image = image
+        self.pixelData = pixelData
+
+def get_pixel_data(bitmapPath: str):
+    numberOfBits: int = 8
+    bitIndex: int = 0 + numberOfBits
+    startIndex: int = 0
+    pixelData: list
+    hexPixels: list = []
+    with Image.open(bitmapPath) as bitmap:
+        pixelData = ["0" if pixel == 0 else "1" for pixel in bitmap.getdata()]
+    while bitIndex < len(pixelData):
+        hexPixels.append(pixelData[startIndex:bitIndex])
+        startIndex += numberOfBits
+        bitIndex += numberOfBits
+    return hexPixels
+
 if __name__ == "__main__":
-    pass
+    testBitmap = BitmapImage(testImage, get_pixel_data(testImage))
+    print(testBitmap.pixelData)
